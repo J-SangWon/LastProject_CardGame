60fd1fa943bff38a7d539801237f66f30758db34
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-07-02 17:39:40 +0900

    07/02

---
+++ LastProject_CardGame/Assets/LJP_Assets/Scripts/GameManager.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonBehaviour<GameManager>
+{
+    private List<GameObject> handCardList = new List<GameObject>();
+}
