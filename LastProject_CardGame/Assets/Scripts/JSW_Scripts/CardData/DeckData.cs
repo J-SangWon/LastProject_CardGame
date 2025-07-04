fb253c1601f37500a5c86173f2e04478bf05de99
J-SangWon <124190818+J-SangWon@users.noreply.github.com>
2025-07-04 14:32:12 +0900

    0704_카드UI + 덱 빌딩 제작중

---
+++ LastProject_CardGame/Assets/Scripts/JSW_Scripts/CardData/DeckData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class DeckData
+{
+    public string deckName;
+    public List<BaseCardData> cards = new List<BaseCardData>();
+}
+
+
---
+++ LastProject_CardGame/Assets/Scripts/JSW_Scripts/DeckBuilder.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DeckBuilder : MonoBehaviour
+{
+    public CardManager cardManager;
+    public DeckData currentDeck;
+
+    // 덱 생성
+    public void CreateDeck(string deckName)
+    {
+        currentDeck = cardManager.CreateDeck(deckName);
+    }
+
+    // 카드 추가
+    public bool AddCard(BaseCardData card)
+    {
+        //중복/최대 수 제한 등 체크 가능
+        if (currentDeck.cards.Count >= 50) return false;
+        currentDeck.cards.Add(card);
+        return true;
+    }
+
+    // 카드 제거
+    public void RemoveCard(BaseCardData card)
+    {
+        currentDeck.cards.Remove(card);
+    }
+
+    // 덱 비우기
+    public void ClearDeck()
+    {
+        currentDeck.cards.Clear();
+    }
+
+    // 덱 저장/불러오기 등 추가 가능
+}
