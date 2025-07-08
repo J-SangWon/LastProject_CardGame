a32895c477b5ba68f72c2eeae5b0f22bfd36e172
J-SangWon <124190818+J-SangWon@users.noreply.github.com>
2025-07-08 14:12:48 +0900

    0708_덱 생성, 수정, 저장, 제거

---
+++ LastProject_CardGame/Assets/Scripts/JSW_Scripts/CardUI/CardDetailUI.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDetailUI : MonoBehaviour
+{
+    public static CardDetailUI Instance;
+
+    public Image cardImage;
+    public TMP_Text nameText;
+    public TMP_Text statText;
+    public TMP_Text descriptionText;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SetCardDetail(BaseCardData cardData)
+    {
+        cardImage.sprite = cardData.artwork;
+        nameText.text = cardData.cardName;
+        statText.text = ""; // 몬스터면 공격력/체력 등 표시
+        descriptionText.text = cardData.description;
+
+        if (cardData is MonsterCardData m)
+        {
+            statText.text = $"공격력: {m.attack} \n체력: {m.health}";
+        }
+    }
+}
