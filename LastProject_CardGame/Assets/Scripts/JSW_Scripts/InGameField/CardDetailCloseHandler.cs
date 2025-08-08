4af36dcbe85abdfc3dc116ac456dcb73ef2fdd8d
J-SangWon <124190818+J-SangWon@users.noreply.github.com>
2025-08-08 14:29:21 +0900

    0808_인게임 카드정보 패널

---
+++ LastProject_CardGame/Assets/Scripts/JSW_Scripts/InGameField/CardDetailCloseHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardDetailCloseHandler : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 좌클릭
+        {
+            if (InGameCardDetailPanel.Instance != null && InGameCardDetailPanel.Instance.IsOpen)
+            {
+                InGameCardDetailPanel.Instance.Hide();
+            }
+        }
+    }
+}
