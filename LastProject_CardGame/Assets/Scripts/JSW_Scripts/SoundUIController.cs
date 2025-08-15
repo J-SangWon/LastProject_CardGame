c66a5f6e603e4ccbb0082e7234700e3cc03bedc0
J-SangWon <124190818+J-SangWon@users.noreply.github.com>
2025-08-15 14:16:47 +0900

    사운드UI스크립트

---
+++ LastProject_CardGame/Assets/Scripts/JSW_Scripts/SoundUIController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundUIController : MonoBehaviour
+{
+    public Slider bgmSlider;
+    public Slider sfxSlider;
+
+    void Start()
+    {
+        // 저장된 볼륨 불러와서 슬라이더 초기화
+        if (SoundManager.Instance != null)
+        {
+            bgmSlider.value = SoundManager.Instance.GetBGMVolume();
+            sfxSlider.value = SoundManager.Instance.GetSFXVolume();
+        }
+
+        // 슬라이더 값 변경 시 사운드 매니저에 적용
+        bgmSlider.onValueChanged.AddListener(OnBGMVolumeChanged);
+        sfxSlider.onValueChanged.AddListener(OnSFXVolumeChanged);
+    }
+
+    void OnBGMVolumeChanged(float value)
+    {
+        if (SoundManager.Instance != null)
+            SoundManager.Instance.SetBGMVolume(value);
+    }
+
+    void OnSFXVolumeChanged(float value)
+    {
+        if (SoundManager.Instance != null)
+            SoundManager.Instance.SetSFXVolume(value);
+    }
+}
